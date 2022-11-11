1464ba7a813ccb8eda3c37f6d5a23e566b6dc256
naraeng <108282052+naraeng@users.noreply.github.com>
2022-11-11 22:34:51 +0900

    Update post_page.dart

--- lib/post_page.dart
+++ lib/post_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'main.dart';
+import 'main_body.dart';
+import 'setting_page.dart';
 
 class PostPage extends StatelessWidget {
   const PostPage({super.key});
@@ -6,6 +9,35 @@ class PostPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: Icon(
+              Icons.arrow_back,
+              color: Colors.black,
+            )),
+        title: Text(
+          'SSCC Talk',
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: sscctalkPrimaryColor,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.settings),
+            color: Colors.black,
+            onPressed: (() {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => SettingPage()),
+              );
+            }),
+          ),
+        ],
+      ),
       body: Center(
         child: Text("Post Page"),
       ),
