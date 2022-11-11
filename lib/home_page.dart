82e3610fc45b104ed92746d018a5695a2dba5d29
naraeng <108282052+naraeng@users.noreply.github.com>
2022-11-11 22:33:10 +0900

    Update home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,11 @@
+//import 'dart:js_util';
+//import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:sscc/notice_page.dart';
+import 'package:sscc/post_page.dart';
+import 'main.dart';
+import 'main_body.dart';
+import 'setting_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,8 +13,164 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'SSCC Talk',
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: sscctalkPrimaryColor,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.settings),
+            color: Colors.black,
+            onPressed: (() {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => SettingPage()),
+              );
+            }),
+          ),
+        ],
+      ),
+      backgroundColor: Colors.white,
       body: Center(
-        child: Text("Home Page"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                Image.asset(
+                  "images/notice.png",
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height / 2.6,
+                  fit: BoxFit.fill,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (_) => NoticePage()));
+                  },
+                  child: Container(
+                    width: MediaQuery.of(context).size.width - 100,
+                    height: MediaQuery.of(context).size.height / 4,
+                    color: Colors.white,
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            'SSCC 홈커밍이 찾아왔습니다! \n 1부 - 2022 해커톤 어쩌고저쩌고 2부 홈커밍데이 어쩌고 11월 19일',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.black,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                        ),
+                        Divider(
+                          color: Colors.grey[800],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            'SSCC 번개모임 안내 \n 일시 : 11/9 수요일 18시, 장소 : 추후 공지 예정',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.black,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                        ),
+                        Divider(
+                          color: Colors.grey[800],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            '해커톤 신청 기한 연장 공지 \n 기존 신청 기한 : 10/31 23:59 -> 변경된 어쩌고',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.black,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                Image.asset(
+                  "images/post.png",
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height / 2.6,
+                  fit: BoxFit.fill,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                        context, MaterialPageRoute(builder: (_) => PostPage()));
+                  },
+                  child: Container(
+                    width: MediaQuery.of(context).size.width - 100,
+                    height: MediaQuery.of(context).size.height / 4,
+                    color: Colors.white,
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            'SSCC 홈커밍이 찾아왔습니다! \n 1부 - 2022 해커톤 어쩌고저쩌고 2부 홈커밍데이 어쩌고 11월 19일',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.black,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                        ),
+                        Divider(
+                          color: Colors.grey[800],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            'SSCC 번개모임 안내 \n 일시 : 11/9 수요일 18시, 장소 : 추후 공지 예정',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.black,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                        ),
+                        Divider(
+                          color: Colors.grey[800],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            '해커톤 신청 기한 연장 공지 \n 기존 신청 기한 : 10/31 23:59 -> 변경된 어쩌고',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.black,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
