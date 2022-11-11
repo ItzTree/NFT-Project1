e293711ae18469ed64d6ee1e95f66f9c7f8ee7cb
naraeng <108282052+naraeng@users.noreply.github.com>
2022-11-11 22:34:03 +0900

    Update main_body.dart

--- lib/main_body.dart
+++ lib/main_body.dart
@@ -1,10 +1,10 @@
-import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 import 'package:flutter/material.dart';
-
-import 'package:sscc_talk/notice_page.dart';
-import 'package:sscc_talk/post_page.dart';
-import 'package:sscc_talk/home_page.dart';
-import 'package:sscc_talk/setting_page.dart';
+import 'package:sscc/post_page.dart';
+import 'package:sscc/setting_page.dart';
+import 'home_page.dart';
+import 'main.dart';
+import 'notice_page.dart';
+import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 
 class MainBody extends StatefulWidget {
   const MainBody({super.key});
@@ -14,34 +14,34 @@ class MainBody extends StatefulWidget {
 }
 
 class _MainBodyState extends State<MainBody> {
-  int currentIndex = 1;
+  int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'SSCC Talk',
-          style: TextStyle(
-            color: Colors.black,
-          ),
-        ),
-        centerTitle: true,
-        elevation: 0,
-        backgroundColor: Colors.white,
-        actions: [
-          IconButton(
-            icon: Icon(Icons.settings),
-            color: Colors.black,
-            onPressed: (() {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (_) => SettingPage()),
-              );
-            }),
-          ),
-        ],
-      ),
+      // appBar: AppBar(
+      //   title: Text(
+      //     'SSCC Talk',
+      //     style: TextStyle(
+      //       color: Colors.black,
+      //     ),
+      //   ),
+      //   centerTitle: true,
+      //   elevation: 0,
+      //   backgroundColor: sscctalkPrimaryColor,
+      //   actions: [
+      //     IconButton(
+      //       icon: Icon(Icons.settings),
+      //       color: Colors.black,
+      //       onPressed: (() {
+      //         Navigator.push(
+      //           context,
+      //           MaterialPageRoute(builder: (_) => SettingPage()),
+      //         );
+      //       }),
+      //     ),
+      //   ],
+      // ),
       body: IndexedStack(
         index: currentIndex,
         children: [
@@ -57,21 +57,21 @@ class _MainBodyState extends State<MainBody> {
   ConvexAppBar customBottomNavBar() {
     return ConvexAppBar(
       style: TabStyle.fixedCircle,
-      backgroundColor: Colors.black45,
-      initialActiveIndex: currentIndex,
+      backgroundColor: sscctalkPrimaryColor,
+
+      // color: Colors.white,
       items: [
         TabItem(
-          icon: Icon(Icons.announcement_outlined, color: Colors.white60),
-          activeIcon: Icon(Icons.announcement_outlined, color: Colors.white),
+          icon: Icon(Icons.announcement_outlined, color: Colors.black),
+          activeIcon: Icon(Icons.announcement_outlined, color: Colors.black),
           title: "공지",
         ),
         TabItem(
-          icon: Icon(Icons.house, size: 36, color: Colors.black26),
-          activeIcon: Icon(Icons.house, size: 36, color: Colors.black45),
+          icon: Icon(Icons.house, size: 36),
         ),
         TabItem(
-          icon: Icon(Icons.question_answer_outlined, color: Colors.white60),
-          activeIcon: Icon(Icons.question_answer_outlined, color: Colors.white),
+          icon: Icon(Icons.question_answer_outlined, color: Colors.black),
+          activeIcon: Icon(Icons.question_answer_outlined, color: Colors.black),
           title: "게시판",
         ),
       ],
