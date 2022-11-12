3e8072e4bc465f9fd9a98424eb0ce8273f1aa6b9
IsaacHa999 <102788008+IsaacHa999@users.noreply.github.com>
2022-11-12 14:51:05 +0900

    Update post_page.dart

--- lib/post_page.dart
+++ lib/post_page.dart
@@ -142,7 +142,7 @@ class _FeedState extends State<Feed> {
               ),
               Spacer(),
               IconButton(
-                icon: Icon(CupertinoIcons.bookmark, color: Colors.black),
+                icon: Icon(CupertinoIcons.delete, color: Colors.black),
                 onPressed: () {},
               ),
             ],
